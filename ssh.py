107887cb8a59ee7f5e1c77084d0e7dc4e9217f1a
Learning Python Code <85742360+LearningCodePython@users.noreply.github.com>
2022-02-07 14:49:25 +0000

    Add files via upload
    app que establece una conexión shh con los ap de unifi

---
+++ ssh.py
@@ -0,0 +1,36 @@
+import paramiko
+ 
+ssh = paramiko.SSHClient()
+know_hosts = paramiko.AutoAddPolicy()
+ssh.set_missing_host_key_policy(know_hosts)
+
+ip = input("¿Direccion IP del AP? ")
+ 
+ssh.connect(
+    hostname= ip,
+    port=22,
+    username="admin",
+    password="!vmaria2017"
+)
+ 
+shell = ssh.invoke_shell()
+shell.settimeout(1)
+ 
+# command = input(">>>") + "\n"
+command = ("\n") # Le envio un enter para acceder directamente al ash
+shell.send(command)
+
+while True:
+    try:
+        recv = shell.recv(512).decode()
+        if recv:
+            print(recv)
+        else:
+            continue
+    except:
+        command = input(">>>") + "\n"
+        shell.send(command)
+
+ssh.close()
+
+# set-inform http://192.168.252.111:8080/inform
